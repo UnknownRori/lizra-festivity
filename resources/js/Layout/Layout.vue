01a5bff66b87783277119abdea239b3dacfab142
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-17 10:14:37 +0700

    📦 Scaffold the basic minimum component

---
+++ resources/js/Layout/Layout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavbarVue from '@/Shared/Navbar.vue';
+import FooterVue from '@/Shared/Footer.vue';
+
+</script>
+
+<template>
+    <NavbarVue />
+    <main>
+        <slot />
+    </main>
+    <FooterVue />
+</template>
--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { Head } from '@inertiajs/vue3'
+
+import LayoutVue from '@/Layout/Layout.vue';
 </script>
 
 <template>
     <Head title="Hello, World" />
-    <h1 class='text-red-500'>Hello, World</h1>
+    <LayoutVue>
+        <h1 class='text-red-500'>Hello, World</h1>
+    </LayoutVue>
 </template>
