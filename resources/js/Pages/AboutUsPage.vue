e171955ba06ada698095ae29d5687a902c1edc03
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-18 06:53:44 +0700

    📦 Scaffold other pages

---
+++ resources/js/Pages/AboutUsPage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Layout from '@/Layout/Layout.vue';
+</script>
+
+<template>
+    <Layout>
+        About us
+    </Layout>
+</template>
+
---
+++ resources/js/Pages/ContactUsPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Layout from '@/Layout/Layout.vue';
+</script>
+
+<template>
+    <Layout>
+        Contact
+    </Layout>
+</template>
