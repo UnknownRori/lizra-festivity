744716f0e1f5b596e26a8b6b80414466db2c4c94
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-18 06:47:21 +0700

    📦 added `SocialMediaLists`

---
+++ resources/js/Data/SocialMediaLists.js
@@ -0,0 +1,13 @@
+import Whatsapp from '@/Assets/Icons/whatsapp.png'
+import Gmail from '@/Assets/Icons/gmail.png'
+import Instagram from '@/Assets/Icons/instagram.png'
+import Facebook from '@/Assets/Icons/facebook.png'
+import Twitter from '@/Assets/Icons/twitter.png'
+
+export default [
+    { src: Whatsapp, alt: 'Whatsapp', href: '#' },
+    { src: Gmail, alt: 'Gmail', href: '#' },
+    { src: Instagram, alt: 'Instagram', href: 'https://www.instagram.com/unknownrori/' },
+    { src: Facebook, alt: 'Facebook', href: '#' },
+    { src: Twitter, alt: 'Facebook', href: 'https://twitter.com/UnknownRori' },
+];
