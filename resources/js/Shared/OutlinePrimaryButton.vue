30821ebeea5d20676e9c662d4c9b87c5ab9f62d5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-18 18:45:39 +0700

    🛠️ Changes default styling

--- resources/js/Shared/OutlinePrimaryButton.vue
+++ resources/js/Shared/OutlinePrimaryButton.vue
@@ -13,11 +13,11 @@ defineEmits(['click']);
 
 <template>
     <Link v-if='$props.type == "anchor"' @click='$emit("click")' :name='$props.href'
-        :class='`border-2 border-red-700 hover:border-red-500 hover:bg-red-500 duration-500 rounded-md text-white p-2 ${$props.class}`'>
+        :class='`border-2 border-red-700 hover:border-red-500 hover:bg-red-500 hover:text-white duration-500 rounded-md p-2 ${$props.class}`'>
     <slot />
     </Link>
     <button v-else @click='$emit("click")'
-        :class='`border-2 border-red-700 hover:border-red-500 hover:bg-red-500 duration-500 rounded-md text-white p-2 ${$props.class}`'>
+        :class='`border-2 border-red-700 hover:border-red-500 hover:bg-red-500 hover:text-white duration-500 rounded-md p-2 ${$props.class}`'>
         <slot />
     </button>
 </template>
