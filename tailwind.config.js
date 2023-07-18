437e6c2a9e76822590d95b80c3551d6b4dc01253
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-18 07:20:00 +0700

    🛠️ Overwrite default font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -7,6 +9,9 @@ module.exports = {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                'sans': ['Averia Sans Libre', ...defaultTheme.fontFamily.sans]
+            },
             screens: {
                 'sm': '240px',
                 'md': '640px',
