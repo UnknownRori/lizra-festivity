c681e7035f7109b5fb4a24e1019c301396669119
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-18 08:26:00 +0700

    📦 Added front cover

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,15 +1,36 @@
 <script setup>
 import { Head } from '@inertiajs/vue3'
 
+import LondonCon from '@/Assets/Images/london-con.jpg';
 import MottoLists from '../Data/MottoLists.js';
 
 import LayoutVue from '@/Layout/Layout.vue';
 import CardVue from '@/Shared/Card.vue';
+import PrimaryButton from '@/Shared/PrimaryButton.vue';
+import OutlinePrimaryButton from '@/Shared/OutlinePrimaryButton.vue';
 </script>
 
 <template>
     <Head title="Hello, World" />
     <LayoutVue>
+        <div class='relative h-[400px]'>
+            <div :class='`front h-[400px] w-full brightness-[25%] absolute -z-10 bg-[url(${LondonCon})]`'>
+                <!--  -->
+            </div>
+            <div class='flex flex-col justify-center items-center h-[400px] gap-4'>
+                <div class='text-3xl text-white w-[45%] text-center'>
+                    <h2>The Power of Imagination Make us Awesome</h2>
+                </div>
+                <div class="flex flex-row gap-2">
+                    <OutlinePrimaryButton href='home' type='anchor'>
+                        Learn More
+                    </OutlinePrimaryButton>
+                    <PrimaryButton>
+                        Join Us
+                    </PrimaryButton>
+                </div>
+            </div>
+        </div>
         <main class='p-12'>
             <div class="flex sm:flex-col lg:flex-row gap-2 justify-evenly">
                 <CardVue v-for='MottoList in MottoLists' class='flex flex-row w-full gap-2 p-2'>
@@ -30,3 +51,11 @@ import CardVue from '@/Shared/Card.vue';
         </main>
     </LayoutVue>
 </template>
+
+<style scoped>
+.front {
+    background-image: url('../Assets/Images/london-con.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+}
+</style>
---
+++ resources/js/Shared/PrimaryButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Link from '@/Shared/Link.vue';
+
+defineProps({
+    class: String,
+    href: String,
+    type: String,
+})
+
+defineEmits(['click']);
+
+</script>
+
+<template>
+    <Link v-if='$props.type == "anchor"' @click='$emit("click")' :name='$props.href'
+        :class='`bg-red-700 hover:bg-red-500 duration-500 rounded-md text-white p-2 ${$props.class}`'>
+    <slot />
+    </Link>
+    <button v-else @click='$emit("click")'
+        :class='`bg-red-700 hover:bg-red-500 duration-500 rounded-md text-white p-2 ${$props.class}`'>
+        <slot />
+    </button>
+</template>
