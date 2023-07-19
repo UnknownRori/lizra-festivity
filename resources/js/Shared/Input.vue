1fbf3c6a052789fc84fd4265c9d6fc02ccbc707f
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-19 20:26:48 +0700

    📦 Implement custom input

---
+++ resources/js/Shared/Input.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['modelValue', 'type', 'placeholder', 'class', 'id']);
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <input :id='$props.id' :type='$props.type' ref='input' @input="$emit('update:modelValue', $event.target.value)"
+        :placeholder='$props.placeholder' :value='modelValue'
+        :class='`border-2 border-gray-200 p-2 rounded-md ${$props.class}`'>
+</template>
