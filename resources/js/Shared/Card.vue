4df6ab302e39a85bf451b666ad97a703b3692ff9
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-20 17:16:18 +0700

    🛠️ make the `class` prop optional

--- resources/js/Shared/Card.vue
+++ resources/js/Shared/Card.vue
@@ -1,11 +1,11 @@
 <script setup lang='ts'>
 defineProps<{
-    class: string,
+    class?: string,
 }>();
 </script>
 
 <template>
-    <div :class='`shadow p-2 m-2 rounded-md border-[1px] border-gray-100 ${$props.class}`'>
+    <div :class='`shadow p-2 m-2 rounded-md border-[1px] border-gray-100 ${$props.class ?? ""}`'>
         <slot />
     </div>
 </template>
