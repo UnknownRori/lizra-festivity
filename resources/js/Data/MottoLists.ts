32a351f25a7b2bf605162cd180577d45efc3d533
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-20 17:15:27 +0700

    🛠️ Fix icon to follow the design

--- resources/js/Data/MottoLists.ts
+++ resources/js/Data/MottoLists.ts
@@ -1,9 +1,9 @@
 import Target from '@/Assets/Icons/target.png';
 import Idea from '@/Assets/Icons/idea.png';
-import Mind from '@/Assets/Icons/mind.png';
+import BrainstormIcon from '@/Assets/Icons/brainstorm.png';
 
 export default [
     { src: Target, alt: 'Target Icon', title: 'Gathering', body: 'Employee Gathering & Family Gathering' },
     { src: Idea, alt: 'Idea Icon', title: 'Inovative', body: 'Meetings, Incentives, Conferences, and Exhibition' },
-    { src: Mind, alt: 'Mind Icon', title: 'Event Organizer', body: 'Development of Personal or Corporate Event' },
+    { src: BrainstormIcon, alt: 'Brainstorm Icon', title: 'Event Organizer', body: 'Development of Personal or Corporate Event' },
 ]
