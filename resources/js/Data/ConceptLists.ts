f1636e33b15afbbeb48828599ea8543b7160c7b8
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-20 17:47:28 +0700

    📦 Added concept list

---
+++ resources/js/Data/ConceptLists.ts
@@ -0,0 +1,24 @@
+import DollarIcon from '@/Assets/Icons/dollar.png'
+import SatisfactionIcon from '@/Assets/Icons/satisfaction.png';
+import MindIcon from '@/Assets/Icons/mind.png';
+
+export default [
+    {
+        src: MindIcon,
+        alt: 'Mind Icon',
+        title: 'Creative Concept',
+        body: 'Lorem ipsum dolor sit amet, consectetur, adipiscing elit, sed do eiusmod, tempor, incididunt, ut labore, er dolore, magna aliqua'
+    },
+    {
+        src: DollarIcon,
+        alt: 'Dollar Icon',
+        title: 'Affordable',
+        body: 'Lorem ipsum dolor sit amet, consectetur, adipiscing elit, sed do eiusmod, tempor, incididunt, ut labore, er dolore, magna aliqua'
+    },
+    {
+        src: SatisfactionIcon,
+        alt: 'Satisfaction icon',
+        title: 'Customer Satisfaction',
+        body: 'Lorem ipsum dolor sit amet, consectetur, adipiscing elit, sed do eiusmod, tempor, incididunt, ut labore, er dolore, magna aliqua'
+    },
+];
