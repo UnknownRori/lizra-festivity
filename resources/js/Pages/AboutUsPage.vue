a275c36dd624adbf9b66da2de1803313ad8f34e5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-20 18:25:54 +0700

    📦 Added more stuff

--- resources/js/Pages/AboutUsPage.vue
+++ resources/js/Pages/AboutUsPage.vue
@@ -1,12 +1,16 @@
 <script setup lang='ts'>
 import { Head } from '@inertiajs/vue3'
 
+import ConceptLists from '@/Data/ConceptLists';
+
 import PresentationEventImg from '@/Assets/Images/presentation-event.jpg';
 import EventImg from '@/Assets/Images/event.jpg';
+import DownloadImg from '@/Assets/Icons/download.png';
 
 import Layout from '@/Layout/Layout.vue';
 import CardVue from '@/Shared/Card.vue';
 import JumpToTopVue from '@/Components/JumpToTop.vue';
+import PrimaryButton from '@/Shared/PrimaryButton.vue';
 </script>
 
 <template>
@@ -21,8 +25,8 @@ import JumpToTopVue from '@/Components/JumpToTop.vue';
                 <h2 class='text-xl'>Lizra Festivity</h2>
             </div>
         </div>
-        <main class='flex flex-col gap-6 p-12'>
-            <section>
+        <main class='flex flex-col gap-6'>
+            <section class='p-12'>
                 <CardVue class='flex lg:flex-row sm:flex-col gap-4 p-2'>
                     <div class='flex flex-col gap-4 w-full'>
                         <div class="flex flex-row py-4 items-center">
@@ -47,10 +51,31 @@ import JumpToTopVue from '@/Components/JumpToTop.vue';
                 </CardVue>
             </section>
 
-            <section>
-                <!--  -->
+            <section class='flex lg:flex-row md:flex-col justify-evenly bg-black text-white p-6 items-center'>
+                <div class='flex gap-2 items-center'>
+                    <h2 class='text-2xl'>Want to know more about us?</h2>
+                    <p class='text-sm'>Just download the brochure</p>
+                </div>
+                <div>
+                    <PrimaryButton class='flex gap-4 items-center'>
+                        <img :src="DownloadImg" alt="download" class='w-8'>
+                        <span>Download Brochure</span>
+                    </PrimaryButton>
+                </div>
             </section>
 
+            <section class='flex lg:flex-row sm:flex-col gap-2 p-14'>
+                <CardVue v-for='concept in ConceptLists'
+                    class='flex flex-col items-center w-full lg:h-72 sm:h-48 text-center'>
+                    <img :src='concept.src' :alt='concept.alt' class='w-[64px]'>
+                    <h2 class='text-3xl font-bold'>
+                        {{ concept.title }}
+                    </h2>
+                    <p>
+                        {{ concept.body }}
+                    </p>
+                </CardVue>
+            </section>
         </main>
     </Layout>
     <JumpToTopVue />
