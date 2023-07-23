1c823e1c54c854247502b8f086702ccf6d2b0156
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-24 03:39:27 +0700

    📦 Added Error Card

---
+++ resources/js/Shared/ErrorCard.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+defineProps<{
+    display: any,
+    body: string,
+}>()
+</script>
+
+<template>
+    <div v-if='$props.display' class='bg-red-500 text-white rounded-md p-2'>
+        {{ $props.body }}
+    </div>
+</template>
