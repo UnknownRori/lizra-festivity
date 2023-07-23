b1d32aa3c2ed83e0568a76f94fcd14fda8aa8d96
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-23 11:41:45 +0700

    📦 Update favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.ts')
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">    @vite('resources/js/app.ts')
     @inertiaHead
 </head>
 
