dfdf32cf41b302baa86d3cf7fd45c7ea8bc07ab2
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-24 07:04:22 +0700

    📦 Added list of news

---
+++ resources/js/Pages/News/Index.vue
@@ -0,0 +1,23 @@
+<script setup lang='ts'>
+import { Head } from '@inertiajs/vue3';
+
+import { NewsPaginator } from '@/types/NewsType';
+
+import Layout from '@/Layout/Layout.vue';
+import NewsListVue from '@/Components/NewsLists.vue';
+
+defineProps<{
+    news: NewsPaginator
+}>();
+</script>
+
+<template>
+    <Head title='Events List' />
+    <Layout>
+        <main>
+            <section class='p-8'>
+                <NewsListVue :news='news.data' />
+            </section>
+        </main>
+    </Layout>
+</template>
