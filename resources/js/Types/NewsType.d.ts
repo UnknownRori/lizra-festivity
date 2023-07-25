9886746ce7c3d5f265aa3f2775c1ad9c3c049ba8
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-25 19:31:28 +0700

    📦 Added more types for `News`

--- resources/js/Types/NewsType.d.ts
+++ resources/js/Types/NewsType.d.ts
@@ -1,17 +1,20 @@
 import { LaravelPaginator } from "./LaravelPaginator";
 
+type PublishStatus = 'Draft' | 'Published';
+
 type NewsType = {
     slug: string,
     thumbnail: string,
     title: string,
     body: string,
     description: string,
+    publish_status?: PublishStatus,
     created_at: string,
     updated_at: string,
 };
 
 type NewsPaginator = LaravelPaginator<NewsType>;
 
-export { NewsPaginator };
+export { NewsPaginator, PublishStatus };
 
 export default News;
