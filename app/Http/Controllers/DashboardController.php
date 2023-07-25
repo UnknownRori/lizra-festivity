5382ed3a6694e3312bf7fa5a05176eac0069551c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-25 16:11:24 +0700

    🛠️ Implement `DashboardController`

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,6 +12,6 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request): Response
     {
-        return inertia('DashboardPage');
+        return inertia('Dashboard/Index');
     }
 }
