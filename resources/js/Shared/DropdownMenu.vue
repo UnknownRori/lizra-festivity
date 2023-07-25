18c68e3f42c748f9627fb302c146577e94cd9798
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-25 17:52:54 +0700

    📦 Added new Dropdown shared component

---
+++ resources/js/Shared/DropdownMenu.vue
@@ -0,0 +1,23 @@
+<script setup lang='ts'>
+import useToggleAnimation from '@/Composable/useToggleAnimation';
+import { None } from '@sniptt/monads';
+
+const { toggle, style, state } = useToggleAnimation({
+    animatedOnStart: true,
+    beforeAnimate: None,
+    offAnimate: 'opacity-0 pointer-events-none',
+    onAnimate: 'opacity-100 pointer-events-auto',
+});
+</script>
+
+<template>
+    <div class='relative' @click='toggle'>
+        <button>
+            <slot name='title' />
+        </button>
+
+        <div :class='`opacity-0 pointer-events-none ${style} duration-500 fixed p-4 bg-white shadow-md`'>
+            <slot />
+        </div>
+    </div>
+</template>
