ca452f5293f7cce0c6f26e37a841ae7115f40721
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-26 09:42:45 +0700

    📦 Allow extending component

--- resources/js/Shared/Input.vue
+++ resources/js/Shared/Input.vue
@@ -1,4 +1,6 @@
 <script setup lang='ts'>
+import { ref } from 'vue';
+
 defineProps<{
     type?: string,
     class?: string,
@@ -10,10 +12,17 @@ defineProps<{
 defineEmits<{
     (e: 'update:modelValue', value: any): void
 }>();
+
+const inputRef = ref(null);
+
+defineExpose({
+    inputRef,
+});
+
 </script>
 
 <template>
-    <input :id='$props.id' :type='$props.type ?? "text"' ref='input'
+    <input ref='inputRef' :id='$props.id' :type='$props.type ?? "text"'
         @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" :placeholder='$props.placeholder'
         :value='modelValue' :class='`border-2 border-gray-200 p-2 rounded-md ${$props.class}`'>
 </template>
